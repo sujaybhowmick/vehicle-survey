8e0a8bd9bf7072f9aaedba74b7f847f3097ddbe6
Sujay Bhowmick <sbhowmick@xactlycorp.com>
2017-01-24 23:52:59 +0530

    created vehicle entry exception class

---
+++ src/main/java/com/aconex/survey/VehicleEntryException.java
@@ -0,0 +1,10 @@
+package com.aconex.survey;
+
+/**
+ * Created by sbhowmick on 1/24/17.
+ */
+public class VehicleEntryException extends RuntimeException {
+    public VehicleEntryException(String s) {
+        super(s);
+    }
+}
