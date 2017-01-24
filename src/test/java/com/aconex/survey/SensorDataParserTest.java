a3cf68fb60f40a8bd7d546ae2cea888eaefd362d
Sujay Bhowmick <sbhowmick@xactlycorp.com>
2017-01-24 23:50:12 +0530

    added test for VehicleEntry Input parser

--- src/test/java/com/aconex/survey/SensorDataParserTest.java
+++ src/test/java/com/aconex/survey/SensorDataParserTest.java
@@ -1,7 +1,8 @@
 package com.aconex.survey;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,23 +14,35 @@ public class SensorDataParserTest {
 
     List<String> sensorInputs;
 
-    @BeforeClass
+    @Before
     public void setUp(){
         this.sensorInputs = new ArrayList<>();
-        this.sensorInputs.add("A86328771");
-        this.sensorInputs.add("B86328774");
-        this.sensorInputs.add("A86328899");
-        this.sensorInputs.add("B86328902");
-        this.sensorInputs.add("A582668");
-        this.sensorInputs.add("B582671");
-        this.sensorInputs.add("A582787");
-        this.sensorInputs.add("A582787");
+        this.sensorInputs.add("A268981");
+        this.sensorInputs.add("A269123");
+        this.sensorInputs.add("A604957");
+        this.sensorInputs.add("B604960");
+        this.sensorInputs.add("A605128");
+        this.sensorInputs.add("B605132");
+        this.sensorInputs.add("A1089807");
+        this.sensorInputs.add("B1089810");
+        this.sensorInputs.add("A1089948");
+        this.sensorInputs.add("B1089951");
+
     }
 
     @Test
     public void testValidInput(){
         SensorDataParser parser = new SensorDataParser();
         List<VehicleEntry> entries = parser.parse(this.sensorInputs);
+        int actual = entries.size();
+        int expected = 3;
+        assertEquals(expected, actual);
+
+        entries = parser.parse(this.sensorInputs.subList(2, sensorInputs.size()));
+        assertEquals(2, entries.size());
+
+        entries = parser.parse(this.sensorInputs.subList(6, sensorInputs.size()));
+        assertEquals(1, entries.size());
 
     }
 }
