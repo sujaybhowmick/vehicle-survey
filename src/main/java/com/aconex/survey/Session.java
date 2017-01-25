db4ec6072976ecee902a439339e8a31c63225ab3
Sujay Bhowmick <sbhowmick@xactlycorp.com>
2017-01-25 16:06:41 +0530

    Added Session class and required method to create sessions given an interval for a day

---
+++ src/main/java/com/aconex/survey/Session.java
@@ -0,0 +1,42 @@
+package com.aconex.survey;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Created by sbhowmick on 1/25/17.
+ */
+public class Session {
+    private final Date startTime;
+
+    private final Date endTime;
+
+    private Session(Date startTime, Date endTime){
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public static List<Session> createSessions(int intervalMins){
+        List<Session> sessions = new ArrayList<>();
+        int minsInDay = TimeUtils.HOURS_IN_A_DAY * TimeUtils.MINUTES_IN_A_HOUR;
+        if(minsInDay % intervalMins != 0){
+            System.out.println(String.format("Interval of %d mins cannot be distributed evenly", intervalMins));
+            return sessions;
+        }
+        int sessionCount = minsInDay / intervalMins;
+        for(int i = 0; i < sessionCount; i++){
+            Date startTime = new Date(i * intervalMins * TimeUtils.SECONDS_IN_A_MINUTE * TimeUtils.MILLISECONDS_IN_A_SECOND);
+            Date endTime = new Date((i+1) * intervalMins * TimeUtils.SECONDS_IN_A_MINUTE * TimeUtils.MILLISECONDS_IN_A_SECOND);
+            sessions.add(new Session(startTime, endTime));
+        }
+        return sessions;
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss", Locale.ENGLISH);
+        sdf.setTimeZone(new SimpleTimeZone(SimpleTimeZone.UTC_TIME, "UTC"));
+
+        return "Session starting from " + sdf.format(startTime) + " ending at " + sdf.format(endTime);
+    }
+}
