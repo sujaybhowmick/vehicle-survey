dcc2e16c6e466c97347f2490ba00087dd858dcd2
Sujay Bhowmick <sbhowmick@xactlycorp.com>
2017-01-26 00:56:45 +0530

    Speed Distribution test passed

--- src/test/java/com/aconex/survey/reports/SpeedDistributionReportServiceTest.java
+++ src/test/java/com/aconex/survey/reports/SpeedDistributionReportServiceTest.java
@@ -1,6 +1,5 @@
 package com.aconex.survey.reports;
 
-import com.aconex.survey.Session;
 import org.junit.Test;
 
 import java.util.List;
@@ -16,10 +15,14 @@ public class SpeedDistributionReportServiceTest extends BaseReportServiceTest {
     @Test
     public void testGenerate(){
         int interval = 720;
-        List<Session> sessions = Session.createSessions(interval);
         SpeedDistributionReportService reportService = new SpeedDistributionReportService(interval);
-        List<SpeedDistributionReportService.SpeedDistributionReportItem> models = reportService.generate(entries);
-        assertEquals();
+        List<SpeedDistributionReportService.SpeedDistributionReportItem> items = reportService.generate(entries);
+        assertEquals(2, items.size());
+        SpeedDistributionReportService.SpeedDistributionReportItem item0 = items.get(0);
+        SpeedDistributionReportService.SpeedDistributionReportItem item1 = items.get(1);
+        assertEquals(59.85, item0.averageSpeed, 00.00521677317741);
+        assertEquals(0.0, item1.averageSpeed,0.0);
+
     }
 
 }
