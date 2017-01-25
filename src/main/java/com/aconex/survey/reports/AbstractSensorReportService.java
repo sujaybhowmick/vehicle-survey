be8296ef9ba33ba3ce22921d40b141b916bf8943
Sujay Bhowmick <sbhowmick@xactlycorp.com>
2017-01-26 00:16:02 +0530

    Added Abstract Report Service class

---
+++ src/main/java/com/aconex/survey/reports/AbstractSensorReportService.java
@@ -0,0 +1,25 @@
+package com.aconex.survey.reports;
+
+import com.aconex.survey.Direction;
+import com.aconex.survey.Session;
+import com.aconex.survey.VehicleEntry;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by sbhowmick on 1/25/17.
+ */
+public abstract class AbstractSensorReportService<T> implements SensorReportService<T> {
+    protected List<VehicleEntry> filterEntriesForSession(List<VehicleEntry> entries, Session session){
+        return entries.stream()
+                .filter(vehicleEntry ->
+                        vehicleEntry.getEntryTime().compareTo(session.startTime()) >= 0
+                        && vehicleEntry.getEntryTime().compareTo(session.endTime()) < 0).collect(Collectors.toList());
+    }
+
+    protected List<VehicleEntry> filterEntriesForDirection(List<VehicleEntry> entries, Direction direction){
+        return entries.stream().filter(vehicleEntry ->
+                vehicleEntry.getDirection() == direction).collect(Collectors.toList());
+    }
+}
